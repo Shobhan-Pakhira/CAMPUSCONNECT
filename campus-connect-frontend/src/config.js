b0fc3735abc466b07b2be70beb8b2977151e57e3
Shivansh-gg1 <shivanshsharma.ait@gmail.com>
2023-04-24 13:33:12 +0530

    added backend deployment link

--- campus-connect-frontend/src/config.js
+++ campus-connect-frontend/src/config.js
@@ -1,3 +1,4 @@
-export const config =  {
-  backendBaseUrl: "http://localhost:3002/"
+export const config = {
+  // backendBaseUrl: "http://localhost:3002/"
+  backendBaseUrl: "https://campusconnect-backend-ait.vercel.app/"
 };
